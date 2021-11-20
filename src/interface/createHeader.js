5f9ecad9e5c5425c2242b96ba3691df9223bdd06
SrivathsanNayak <ganeshsrivathsan@gmail.com>
2021-11-20 15:14:40 +0530

    Added search button

--- src/interface/createHeader.js
+++ src/interface/createHeader.js
@@ -4,15 +4,27 @@ function createNavBar() {
     const searchDiv = document.createElement("div");
     const modeSwitch = document.createElement("div");
 
+    //Logo and title
     const logo = document.createElement("span");
     logo.textContent = "playlist_add_check_circle";
     logo.classList.add('material-icons', 'logo');
-    
     const title = document.createElement("h1");
-    title.textContent = "ToDoList";
+    title.textContent = "Todo-List";
     title.classList.add('title');
-
+    logoTitle.classList.add('logo-title');
     logoTitle.append(logo, title);
+
+    //Search field
+    const searchInput = document.createElement("input");
+    searchInput.placeholder = "Search..";
+    const searchButton = document.createElement("button");
+    const searchIcon = document.createElement("span");
+    searchIcon.textContent = "search";
+    searchIcon.classList.add('material-icons', 'search');
+    searchButton.classList.add('search-button');
+    searchButton.append(searchIcon);
+    searchDiv.append(searchInput, searchButton);
+
     navBar.append(logoTitle, searchDiv, modeSwitch);
 
     return navBar;
