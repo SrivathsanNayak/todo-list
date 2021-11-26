f9426b9e9dcbd8c06a3501cb1517e25c3d26fd33
SrivathsanNayak <ganeshsrivathsan@gmail.com>
2021-11-26 07:53:27 +0530

    Added button to collapse sidebar

--- src/interface/createMain.js
+++ src/interface/createMain.js
@@ -1,15 +1,29 @@
 function createMain() {
     const main = document.createElement("div");
+    main.classList.add('main-body');
     
     const sideBar = document.createElement("div");
     const todayDiv = document.createElement("div");
     sideBar.id = "side-bar";
     todayDiv.id = "today-div";
 
-    // sideBar.textContent = "Hi";
-    // todayDiv.textContent = "Hello";
+    const collapseSideBar = document.createElement("span");
+    collapseSideBar.textContent = "swap_horizontal_circle";
+    collapseSideBar.classList.add('material-icons', 'collapse-side-bar');
 
-    main.append(sideBar, todayDiv);
+    /*Event listener for collapse button*/
+    collapseSideBar.addEventListener('click', () => {
+        if (sideBar.classList.contains('show-side-bar')) {
+            sideBar.classList.remove('show-side-bar');
+        } else {
+            sideBar.classList.add('show-side-bar');
+        }
+    });
+
+    sideBar.textContent = "Hi";
+    todayDiv.textContent = "Hello";
+
+    main.append(sideBar, collapseSideBar, todayDiv);
 
     return main;
 }
